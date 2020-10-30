c56bfd0ce16f2e83e599876adbbc9a6a403552a8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-30 21:01:24 +0700

    Change the style

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,11 +1,5 @@
 const main = () => {
-    let body = document.getElementsByTagName('body');
-    let font = document.createElement('font');
-    let message = document.createTextNode('Hello, World!');
 
-    font.appendChild(message);
-    body[0].appendChild(font);
-    font.setAttribute('color', 'red');
 }
 
 export default main;
