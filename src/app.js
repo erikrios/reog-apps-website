2e33b860b477181a5499aa30eeb5607260927572
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-30 22:24:36 +0700

    Import font awesome

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import 'regenerator-runtime';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import './styles/style.css';
 import main from './scripts/view/main.js';
 document.addEventListener('DOMContentLoaded', main);
