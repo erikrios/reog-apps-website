dd556c78dee9a47373c503dfbd9d75f6e9aac023
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-31 02:29:58 +0700

    Create data-source.js

---
+++ src/scripts/data/data-source.js
@@ -0,0 +1,23 @@
+class DataSource {
+    static authenticate(email, username) {
+        return fetch('https://reog.herokuapp.com/api/auth', {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then(response => { return response.json() })
+            .then(responseJson => {
+                if (responseJson.status.contains('success')) {
+                    return Promise.resolve(responseJson.data[0]);
+                } else {
+                    return Promise.reject(`${responseJson.message}`);
+                }
+            })
+            .catch(err => console.error(err));
+    }
+}
