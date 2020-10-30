8dff5423e75d5a96c1b3f22c0f3b0cc6478c6b49
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-30 19:27:46 +0700

    Bugs fixing

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.css$/,
                 use: [
                     {
                         loader: 'style-loader'
