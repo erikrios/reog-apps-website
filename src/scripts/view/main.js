5298bc2731a222b475dcb41eaa1b0ee30c963bd2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-10-31 11:51:26 +0700

    Get authentication token

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,27 +1,44 @@
 import Icon from '../../assets/peacock.svg';
 import DataSource from '../data/data-source.js';
+import cookie from '../data/cookie.js';
+
 const main = () => {
-    const images = document.getElementsByTagName('img');
-    images[0].setAttribute('src', Icon);
 
-    const inputEmail = document.getElementById('input-email');
-    const inputPassword = document.getElementById('input-password');
-    const signInButton = document.getElementById('signin-button');
+    let isLoggedIn;
+    const token = cookie.getCookie('auth-token');
+    if (token) isLoggedIn = true; else isLoggedIn = false;
 
-    inputEmail.addEventListener('keyup', (event) => { onEnter(event, signInButton) });
-    inputPassword.addEventListener('keyup', (event) => { onEnter(event, signInButton) });
+    if (!isLoggedIn) {
+        const images = document.getElementsByTagName('img');
+        images[0].setAttribute('src', Icon);
 
-    signInButton.addEventListener('click', async () => {
-        const email = inputEmail.value;
-        const password = inputPassword.value;
+        const inputEmail = document.getElementById('input-email');
+        const inputPassword = document.getElementById('input-password');
+        const signInButton = document.getElementById('signin-button');
 
-        try {
-            const token = await DataSource.authenticate(email, password);
-            alert(`Token: ${token}`);
-        } catch (error) {
-            alert(`Error: ${error}`)
-        }
-    })
+        inputEmail.addEventListener('keyup', (event) => { onEnter(event, signInButton) });
+        inputPassword.addEventListener('keyup', (event) => { onEnter(event, signInButton) });
+
+        signInButton.addEventListener('click', async () => {
+            const email = inputEmail.value;
+            const password = inputPassword.value;
+
+            try {
+                const token = await DataSource.authenticate(email, password);
+                cookie.setCookie('auth-token', token, 1);
+            } catch (error) {
+                alert(`Error: ${error}`)
+            }
+        });
+    } else {
+        const bodyElement = document.getElementsByTagName('body');
+        bodyElement.removeChild();
+        const tokenNode = document.createTextNode(`Already logged in with token : ${cookie.getCookie('auth-token')}`);
+        const paragraphElement = document.createElement('p');
+        paragraphElement.appendChild(tokenNode);
+        bodyElement.appendChild(paragraphElement);
+    }
+
 }
 
 const onEnter = (event, triggeredElement) => {
